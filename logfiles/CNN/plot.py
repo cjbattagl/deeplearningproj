7934cc11e66c215899b25d79c9091f3ef9b90c0d
Casey Battaglino <cjbattagl@gmail.com>
2016-04-20 13:04:07 -0400

    all plots

--- logfiles/CNN/plot.py
+++ logfiles/CNN/plot.py
@@ -2,22 +2,35 @@ import matplotlib
 matplotlib.use('Agg')
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+from pylab import rcParams
+rcParams['figure.figsize'] = 6, 5
+
+fontP = FontProperties()
+fontP.set_size('xx-small')
+#legend([plot1], "title", prop = fontP)
+
 
 def f(t):
     return np.exp(-t) * np.cos(2*np.pi*t)
 plt.clf()
 models=('NIN/','ResNet/')
-styles=('k--','k','r--','r')
+modelnames=('NIN','ResNet')
+styles=('k','k','r','r','b','b','g','g','c','c','m','m','y','y','k','k','r.','r','b.','b','g.','g','c.','c')
 count = 0
+#plt.figure(num=None, figsize=(8, 6), dpi=80)
+plt.figure(1)
+plt.yticks(np.arange(0, 101, 5.0))
 for mod in models:
+  modname = modelnames[count/2]
   testdata = np.genfromtxt(mod + 'test.log')
   traindata = np.genfromtxt(mod + 'train.log')
   numiters = max(len(testdata),len(traindata))
   t1 = np.arange(0, len(testdata), 1)
   t2 = np.arange(0, len(traindata), 1)
-  plt.figure(1)
-  plt.plot(t1, testdata, styles[count], label=mod+'test',linewidth=1)
-  plt.plot(t2, traindata, styles[count+1], label=mod+'train', linewidth=3)
+  plt.plot(t1, testdata, styles[count], label=modname+'/test',linewidth=2)
+  plt.plot(t2, traindata, styles[count+1], label=modname+'/train', linewidth=1)
   count+=2
-plt.legend()
+plt.legend(loc=8,ncol=4,prop=fontP)
+#loc='upper center', bbox_to_anchor=(0.5, 1.05)
 plt.savefig("out.png", bbox_inches="tight")
--- logfiles/TCNN/plot.py
+++ logfiles/TCNN/plot.py
@@ -3,6 +3,7 @@ matplotlib.use('Agg')
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
+from matplotlib.pyplot import cm
 
 fontP = FontProperties()
 fontP.set_size('xx-small')
@@ -16,9 +17,11 @@ models=('NIN/1L/','NIN/2L/1D/h20-50-250/','NIN/2L/1D/h32-64-256/','NIN/2L/2D/h20
 modelnames=('NIN-1L','NIN-2L-1D-20-50-250','NIN-2L-1D-32-64-256','NIN-2L-2D-20-50-250','ResNet-1L','ResNet-2L')
 styles=('k','k','r','r','b','b','g','g','c','c','m','m','y','y','k','k','r.','r','b.','b','g.','g','c.','c')
 count = 0
-
+numcols = 6
+color=iter(cm.rainbow(np.linspace(0,1,numcols)))
 plt.yticks(np.arange(0, 101, 5.0))
 for mod in models:
+  c = next(color)
   modname = modelnames[count/2]
   testdata = np.genfromtxt(mod + 'test.log')
   traindata = np.genfromtxt(mod + 'train.log')
@@ -26,8 +29,8 @@ for mod in models:
   t1 = np.arange(0, len(testdata), 1)
   t2 = np.arange(0, len(traindata), 1)
   plt.figure(1)
-  plt.plot(t1, testdata, styles[count], label=modname+'/test',linewidth=2)
-  plt.plot(t2, traindata, styles[count+1], label=modname+'/train', linewidth=1)
+  plt.plot(t1, testdata, c=c, label=modname+'/test',linewidth=2)
+  plt.plot(t2, traindata, c=c, label=modname+'/train', linewidth=1)
   count+=2
 plt.legend(loc=8,ncol=4,prop=fontP)
 #loc='upper center', bbox_to_anchor=(0.5, 1.05)
